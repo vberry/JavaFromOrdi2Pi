e830940c32b17e90d9e406981c298a5a7c0c5133
Vincent Berry <vberry@lirmm.fr>
2016-03-27 10:37:25 +0200

    Changé les messages affichés + fermé le scanner (warning Eclispe)

--- TestLaunchPi/src/Main.java
+++ TestLaunchPi/src/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Raspberry PI");
+		System.out.println("Hello Raspberry PI. Entrez un mot : ");
 		Scanner clavier = new Scanner(System.in);
 		String message = clavier.next();
-		System.out.println("Message donne par le clavier du RaspBerry : ");
+		clavier.close();
+		System.out.print("Prochain mot obtenu du clavier : ");
 		System.out.println(message);
 	}
 
